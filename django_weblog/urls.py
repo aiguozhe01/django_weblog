ae22b0ab1e9bba48cc1a490b29dcf52d04fa7001
Hiro Saito <aiguozhe01@gmail.com>
2020-03-20 10:45:13 +0900

    Added register.html with few modifications.

--- django_weblog/urls.py
+++ django_weblog/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
 
 # This urls are for the entrance to your blog only. Once this sends to the blog, it is done.
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'),
     path('', include('blog.urls')), # The empty path means, the homepage will be under localhost:800
 ]
