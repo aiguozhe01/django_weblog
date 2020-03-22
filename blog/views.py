797cbafb9ccae475982905ef987d7d4a8542b37f
Hiro Saito <aiguozhe01@gmail.com>
2020-03-22 13:17:44 +0900

    Added classes of PostListView and PostDetailView.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post # from models.py, import Post
 
 def home(request):
@@ -6,6 +7,15 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts' # overriding the default name object_list instead of post.
+    ordering = ['-date_posted'] # argument to order the posts.
+
+class PostDetailView(DetailView):
+    model = Post
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
