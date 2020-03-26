eef38d9a4d8a33e16ccd47a7bee94e8da719994e
Hiro Saito <aiguozhe01@gmail.com>
2020-03-26 20:09:46 +0900

    Added password_reset_confirm and done pages.

--- django_weblog/urls.py
+++ django_weblog/urls.py
@@ -29,6 +29,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'), # Django will handel the form and backend stuff.
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'), # We still need to make a template.
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'), # We still need to make a template.  
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'), # We still need to make a template.  
+    path('password-reset-confrim/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'), # We still need to make a template.  
     path('', include('blog.urls')), # The empty path means, the homepage will be under localhost:800
 ]
 
